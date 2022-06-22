ac24350d58e8ba61e475b0818f55b67e530aa007
tunalime <mfethi96@gmail.com>
2022-06-22 18:51:39 +0300

    queries fixed.

--- src/main/java/com/example/week4/dao/AdvertisementRepository.java
+++ src/main/java/com/example/week4/dao/AdvertisementRepository.java
@@ -2,6 +2,8 @@ package com.example.week4.dao;
 import com.example.week4.entity.Advertisement;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
 import java.util.Date;
 import java.util.List;
 
@@ -16,8 +18,8 @@ public interface AdvertisementRepository extends JpaRepository<Advertisement, Lo
     @Query(value = "select * from advertisement where price between ?1 and ?2 order by price asc", nativeQuery = true)
     List<Advertisement> findByPrice(Long min, Long max);
 
-    @Query(value = "select * from advertisement where title like '%?1%' or detailed_message like '%?1%' order by created_at desc", nativeQuery = true)
-    List<Advertisement> findByTitleOrDetailedMessage(String text);
+    @Query(value = "select * from advertisement where title like %:text% or detailed_message like %:text% order by created_at desc", nativeQuery = true)
+    List<Advertisement> findByTitleOrDetailedMessage(@Param("text") String text);
 
 
 }
